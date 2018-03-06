18283ef6d6d0503f1c8713b682d3ccfef015a562
Sven Andresen <s.andresen@lmsoftware.de>
2018-03-06 18:11:05 +0100

    fixes in odometrie using the kin modell and using mvnrnd

--- odometrie.m
+++ odometrie.m
@@ -1,30 +1,10 @@
-function [pos] = odometrie(pos_old, v, noiseFactor)
-    %Radius Achsenlaenge
-    L = 0.1575;
-    %Geschwindigkeit Roboter
-    vel = v(1,1);
-    %Winkelgeschwindigkeit
-    w = v(1,2);
-    %delta t, Zeit seit letztem Schritt
-    t = v(1,3);
-    %Radgeschwindigkeiten
-    vleft = vel+L*w +rand(1)*noiseFactor;
-    vright = L*w-vel +rand(1)*noiseFactor;
+function [pos] = odometrie(pos_old, v, sigma)
+    %normal distribution around 0 with sigma
+    mu = 0;
+    r = mvnrnd(mu, sigma, 2);
+    v(1,1) = v(1,1) + r(1);
+    v(1,2) = v(1,2) + r(2);
     
-    %Strecke links, rechts
-    l_L = vleft*t;
-    l_R = vright*t;
-    
-    %Änderung Strecke
-    delta_s = (l_R - l_L)/2;
-    
-    %Änderung Orientierung
-    delta_phi = mod((l_R+l_L)/(2*L) + pos_old(1,3), 2*pi);
-    
-    %Neue Position berechnen.
-    pos(1,1) = pos_old(1,1) + delta_s*cos(delta_phi);
-    pos(1,2) = pos_old(1,2) + delta_s*sin(delta_phi);
-    pos(1,3) = delta_phi;
-
+    pos = kinModell(pos_old, v);
 end
 
